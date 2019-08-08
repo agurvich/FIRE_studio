b11af839ddb411b4615d84c3faa8301392ab5513
Alex Gurvich <agurvich@u.northwestern.edu>
2019-08-07 21:43:29 -0500

    change testit path to snaps...

--- testit.sh
+++ testit.sh
@@ -1,2 +1,2 @@
 curdir=`pwd`
-python firestudio/gas_movie_maker.py --snapdir="${HOME}/research/snaps/m12i_res7100/output" --snapstart=600 --snapmax=600 --frame_half_width=5 --frame_depth=5 --datadir=${curdir} --multiproc=0 --extract_galaxy=1 --noaxis=1 --edgeon=0 --pixels=1200
+python firestudio/gas_movie_maker.py --snapdir="${HOME}/snaps/m12i_res7100/output" --snapstart=600 --snapmax=600 --frame_half_width=5 --frame_depth=5 --datadir=${curdir} --multiproc=0 --extract_galaxy=1 --noaxis=1 --edgeon=0 --pixels=1200
