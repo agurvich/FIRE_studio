7849b89aec66f959034801ad6cfe9146d078e929
Alex Gurvich <agurvich@u.northwestern.edu>
2020-09-01 17:47:24 -0500

    replace C backend for GasStudio.

--- firestudio/utils/gas_utils/HsmlAndProject_cubicSpline/main.c
+++ firestudio/utils/gas_utils/HsmlAndProject_cubicSpline/main.c
@@ -3,322 +3,166 @@
 #include <string.h>
 #include <math.h>
 
-#include "proto.h"
-#include "allvars.h"
-
-int findHsmlAndProject(int _NumPart, struct particle_data* _P, float* _Hsml, float* _Mass, float* _Quantity, float _Xmin, float _Xmax, float _Ymin, float _Ymax, float _Zmin, float _Zmax, int _Xpixels, int _Ypixels, int _DesDensNgb, int _Axis1, int _Axis2, int _Axis3, float _Hmax, double _BoxSize, float* _Value, float* _ValueQuantity);
-
-int findHsmlAndProject(int _NumPart, struct particle_data* _P,
-		       float* _Hsml, float* _Mass, float* _Quantity,
-		       float _Xmin, float _Xmax, float _Ymin, float _Ymax, float _Zmin, float _Zmax,
-		       int _Xpixels, int _Ypixels, int _DesDensNgb, int _Axis1, int _Axis2, int _Axis3,
-		       float _Hmax, double _BoxSize, float* _Value, float* _ValueQuantity)
-
-{
- 
-  NumPart = _NumPart;
-  P = _P;
-  Hsml = _Hsml;
-  Mass = _Mass;
-  Quantity = _Quantity;
-  Xmin = _Xmin;
-  Xmax = _Xmax;
-  Ymin = _Ymin;
-  Ymax = _Ymax;
-  Zmin = _Zmin;
-  Zmax = _Zmax;
-  Xpixels = _Xpixels;
-  Ypixels = _Ypixels;
-  DesDensNgb =  _DesDensNgb;
-  Axis1 = _Axis1;
-  Axis2 = _Axis2;
-  Axis3 = _Axis3;
-  Hmax = _Hmax;
-  BoxSize = _BoxSize;
-  Value =         _Value;
-  ValueQuantity = _ValueQuantity;
-
-  /*fprintf(stdout, "NumPart = %d\n", NumPart);
-  fprintf(stdout, "P = %p \n", P);
-  fprintf(stdout, "Hsml = %p \n", Hsml);
-  fprintf(stdout, "Mass = %p \n", Mass);
-  fprintf(stdout, "Quantity = %p \n", Quantity);
-  fprintf(stdout, "Xmin = %f, Xmax = %f \n", Xmin,Xmax);
-  fprintf(stdout, "Ymin = %f, Ymax = %f \n", Ymin,Ymax);
-  fprintf(stdout, "Zmin = %f, Zmax = %f \n", Zmin,Zmax);
-  fprintf(stdout, "Xpixels = %d, Ypixels = %d \n", Xpixels,Ypixels);
-  fprintf(stdout, "DesDensNgb = %d \n", DesDensNgb);
-  fprintf(stdout, "Axis1 = %d, Axis2 = %d, Axis3 = %d\n", Axis1,Axis2,Axis3);
-  fprintf(stdout, "Hmax = %f \n", Hmax);
-  fprintf(stdout, "BoxSize = %g \n", BoxSize);
-  fprintf(stdout, "Value = %p\n", Value);
-  fprintf(stdout, "ValueQuantity = %p\n", ValueQuantity);
-
-  printf("peano-hilbert order...\n");*/
-  peano_hilbert_order();
-  /*printf("done\n");*/
-
-  if (NumPart < 100000) {
-    //printf(" allocating memory for %d tree nodes\n",100*NumPart);
-      tree_treeallocate(100*NumPart, NumPart);
-  } else {
-    //printf(" allocating memory for %d tree nodes\n",100*NumPart);
-      tree_treeallocate(100*NumPart, NumPart);
-    }
-  //Softening = (Xmax-Xmin)/Xpixels/100.;
-  Softening = (Xmax-Xmin)/pow(NumPart,0.3333)/10.0;
-
-  //printf("build tree...\n");
-  tree_treebuild();
-  //printf("done.\n");
-
-  //printf("finding neighbours...\n");
-  determine_hsml();
-  //printf("done.\n");
-
-  tree_treefree();
-    
-  //printf("projecting\n");
-  make_map();
-  //printf("done\n");
-    
-  // find min/max of arrays
-  /*float minval = 1.0e20;
-  float maxval = -minval;
-  float minvalQ = 1.0e20;
-  float maxvalQ = -minvalQ;
-  int nelements = Xpixels*Ypixels;
-  int i;
-  for (i = 0; i < nelements; i++)
-    {
-      if (Value[i] > maxval)
-	{
-	  maxval = Value[i];
-	}
-      else if (Value[i] < minval)
-	{
-	  minval = Value[i];
-	}
+/* 
+    this routine takes a set of points and does an approximate projection through them, 
+        weighting the quantity by the weight. Returns sum(weight) in pixel to OUT0 and 
+        sum(weight*quantity) in pixel to OUT1.
       
-      if (ValueQuantity[i] > maxvalQ)
-	{
-	  maxvalQ = ValueQuantity[i];
-	}
-      else if (ValueQuantity[i] < minvalQ)
-	{
-	  minvalQ = ValueQuantity[i];
-	}    
-      }    
+*/
+
+/* extremely fast approximation function for the exponential, 
+    useful here since fractional accuracy errors are smaller than the kernel sources anyways */
+inline double fast_exp(double y) {
+    double d;
+    *((int*)(&d) + 0) = 0;
+    *((int*)(&d) + 1) = (int)(1512775 * y + 1072632447);
+    return d;
+}
+
+// changed to better fit python wrapper, not IDL //
+int hsml_project(
+    int N_xy, // number of input particles/positions
+    float* x, float* y, // positions (assumed already sorted in z)
+    float* hsml, // smoothing lengths for each
+    float* weight, // weight 
+    float* quantity, // quantity
+    float Xmin, float Xmax, float Ymin, float Ymax, // boundaries of output grid
+    int Xpixels, int Ypixels, // dimensions of grid
+    float* OUT0, float* OUT1) // output vectors for weightMap and weightWeightedQuantityMap
+{
+  // print out the input parameters // 
+  printf("N_xy=%d...",N_xy); 
+  printf("Xmin=%f...Xmax=%f...Ymin=%f...Ymax=%f...Xpixels=%d...Ypixels=%d\n",
+    Xmin,Xmax,Ymin,Ymax,Xpixels,Ypixels);
+  printf("quantity=%f...\n",quantity);
+
+  double dx, dy, dx_i, dy_i, dx_n, dy_n, i_x_flt, i_y_flt, d_ij, h, hmax, hmin;
+  double h2, x2_n, y2_n, r2_n, h2_i, wk, wt_sum, hkernel_over_hsml_to_use, *Kernel; 
+  double dpi=3.1415926535897932384626433832795;
+  long n,i,j,k,imin,imax,jmin,jmax,N_KERNEL_TABLE;
   
-    fprintf(stdout, "min(Value) = %f, max(Value) = %f\n", minval,maxval);
-    fprintf(stdout, "min(ValueQuantity) = %f, max(ValueQuantity) = %f\n", minvalQ,maxvalQ);*/
-
-
-  return 0;
-}
-
-void determine_hsml(void)
-{
-  int i, signal;
-  double h;
-
-  for(i = 0, signal = 0, h = 0; i < NumPart; i++)
+  dx = (Xmax - Xmin)/((double)Xpixels);
+  dy = (Ymax - Ymin)/((double)Ypixels);
+  dx_i = 1./dx; dy_i = 1./dy;
+  hmax = 100.*sqrt(dx*dx+dy*dy); // set this purely to prevent going over too many cells //
+  hmin = 0.5*sqrt(dx*dx+dy*dy); // ensures at least one cell 'sees' the particle // 
+  
+  // pre-define cell positions so we save a step in the loop //
+  double x_i[Xpixels]; for(i=0;i<Xpixels;i++) x_i[i]=Xmin+dx*((double)i+0.5);
+  double y_j[Ypixels]; for(i=0;i<Ypixels;i++) y_j[i]=Ymin+dy*((double)i+0.5);
+  
+  // build a kernel lookup table to save on the calculation below // 
+  hkernel_over_hsml_to_use = 1.0; 
+  //hkernel_over_hsml_to_use = 2.0; 
+  // default = 1 (integrate out to kernel), but low-density regions are represented 
+  //   more accurately if this is larger (~2); code expense increases though!
+  N_KERNEL_TABLE = 1000;
+  Kernel = calloc(N_KERNEL_TABLE+1, sizeof(double)); 
+  dx_n=(hkernel_over_hsml_to_use*hkernel_over_hsml_to_use)/((double)N_KERNEL_TABLE); r2_n=0.;
+  double kernel_spacing_inv = 1./dx_n;
+  for(n=0;n<N_KERNEL_TABLE;n++)
+  {
+   h = sqrt(r2_n); // radius (to save sqrt operation we're interpolating in r^2 //
+   // approximate gaussian for projected, integrate kernel: //
+        //wk = (135./(14.*dpi)) * exp(-(135./14.)*h*h); // quintic spline we're using now   
+        //wk = (135./(14.*dpi)) * exp(-(135./14.)*h*h/(1.+h)); 
+        // this has more extended tails, designed to reduce artifacts in low-density regions
+        //  (where we would really want to use a proper volume-render)
+        // wk = 1.91 * exp(-5.56*h*h); // cubic spline 
+   // cubic spline kernel
+        h2=(1.-h); if(h<=0.5) {wk=(1.-6.*h*h*h2);} else {wk=2.*h2*h2*h2;} wk*=8/dpi; //wk*=40./(7.0*dpi);
+   Kernel[n] = wk;
+   r2_n += dx_n; // radius at this point in the table
+  }
+  Kernel[N_KERNEL_TABLE]=0;
+  
+  // zero out the output vectors before the main sum
+  for(n=0;n<Xpixels*Ypixels;n++)
+  {
+    OUT0[n]=0.0; OUT1[n]=0.0;
+  }
+  
+  // loop over particles // 
+  for(n=0;n<N_xy;n++)
+  {
+    i_x_flt = (x[n] - Xmin) * dx_i;
+    i_y_flt = (y[n] - Ymin) * dy_i;
+    h = hsml[n]; if(h<hmin) h=hmin; // assume 'intrinsic' h is smeared by some fraction of pixel
+    h2_i = 1./(h*h); // here we need the 'real' h (not the expanded search) // 
+    h *= hkernel_over_hsml_to_use; // make search area larger for kernel //
+    if(h > hmax) h=hmax; h2 = h*h; // this uses the expanded search radius // 
+    d_ij=h*dx_i; imin=(long)(i_x_flt-d_ij); imax=(long)(i_x_flt+d_ij)+1; if(imin<0) imin=0; if(imax>Xpixels-1) imax=Xpixels-1;
+    d_ij=h*dy_i; jmin=(long)(i_y_flt-d_ij); jmax=(long)(i_y_flt+d_ij)+1; if(jmin<0) jmin=0; if(jmax>Ypixels-1) jmax=Ypixels-1;
+    
+    wt_sum = 0.;
+    // ABG do initial loop to count total weight deposited
+    for(i=imin;i<imax;i++)
     {
-      if(i > (signal / 100.0) * NumPart)
+     dx_n = x[n]-x_i[i]; 
+     if (fabs(dx_n) < h)
+     {
+     x2_n = dx_n*dx_n;
+     for(j=jmin;j<jmax;j++)
+     {
+       dy_n = y[n]-y_j[j]; 
+       y2_n = dy_n*dy_n;
+       r2_n = x2_n + y2_n; 
+       if (r2_n < h2) 
+       {
+        // use the kernel lookup table compiled above for the weighting //
+        r2_n *= h2_i*kernel_spacing_inv; // ok now have the separation in units of hsml, then kernel_table // 
+        k = (long)r2_n;
+        wk = h2_i * (Kernel[k] + (Kernel[k+1]-Kernel[k])*(r2_n-k)); // ok that's the weighted result
+
+        k = j + Ypixels*i; // j runs 0-Ypixels-1, so this provides the necessary indexing //
+        wt_sum += wk;
+       }
+     }
+     }
+    }
+    for(i=imin;i<imax;i++)
         {
-          /*printf("x");
-	    fflush(stdout);*/
-          signal++;
-        }
+         dx_n = x[n]-x_i[i]; 
+         if (fabs(dx_n) < h)
+         {
+         x2_n = dx_n*dx_n;
+         for(j=jmin;j<jmax;j++)
+         {
+           dy_n = y[n]-y_j[j]; 
+           y2_n = dy_n*dy_n;
+           r2_n = x2_n + y2_n; 
+           if (r2_n < h2) 
+           {
+            // use the kernel lookup table compiled above for the weighting //
+            r2_n *= h2_i*kernel_spacing_inv; // ok now have the separation in units of hsml, then kernel_table // 
+            k = (long)r2_n;
+            wk = h2_i * (Kernel[k] + (Kernel[k+1]-Kernel[k])*(r2_n-k)); // ok that's the weighted result
 
-      if(Hsml[i] == 0)
-        Hsml[i] = h  = ngb_treefind(P[i].Pos, DesDensNgb, h * 1.1);
-    }
+            // ABG: renormalize by sum of wk
+            wk = wk/wt_sum;
 
-  //printf("\n");
-}
+            k = j + Ypixels*i; // j runs 0-Ypixels-1, so this provides the necessary indexing //
+        
+            OUT0[k] += weight[n]*wk;
+            OUT1[k] += weight[n]*wk*quantity[n];
 
+           } // if (r2_n < h2)
+        } // for(j=jmin;j<jmax;j++)
+      } // if (x2_n < h2)
+    } // for(i=imin;i<imax;i++)
 
-#ifdef PERIODIC
-#define NGB_PERIODIC(x) (((x)>BoxHalf)?((x)-BoxSize):(((x)<-BoxHalf)?((x)+BoxSize):(x)))
-#else
-#define NGB_PERIODIC(x) (x)
-#endif
-
-
-
-void make_map(void)
-{
-  int i, j, n;
-  int dx, dy, nx, ny;
-  double h, r, u, wk;
-  double pos[2];
-  double LengthX;
-  double LengthY;
-  double r2, h2;
-  double sum, hmin, hmax, x, y, xx, yy, xxx, yyy;
-  double pixelsizeX, pixelsizeY;
-
-  BoxHalf = 0.5 * BoxSize;
-
-  for(i = 0; i < Xpixels; i++)
-    for(j = 0; j < Ypixels; j++)
-      {
-        Value[i * Ypixels + j] = 0;
-        ValueQuantity[i * Ypixels + j] = 0;
-      }
- 
-
-  LengthX = Xmax-Xmin;
-  LengthY = Ymax-Ymin;
-
-  Xc = 0.5 * (Xmax + Xmin);
-  Yc = 0.5 * (Ymax + Ymin);
-
-  pixelsizeX = LengthX / Xpixels;
-  pixelsizeY = LengthY / Ypixels;
-
-  if(pixelsizeX < pixelsizeY)
-    hmin = 1.001 * pixelsizeX / 2;
-  else
-    hmin = 1.001 * pixelsizeY / 2;
-
-
-  hmax = Hmax;
-
-
-  for(n = 0; n < NumPart; n++)
+    if(!(n%10000))
     {
-      /*if((n % (NumPart / 100)) == 0)
-	{
-	  printf(".");
-	  fflush(stdout);
-	  }*/
-
-      if(P[n].Pos[Axis3]< Zmin || P[n].Pos[Axis3] > Zmax)
-        continue;
-
-      pos[0]= P[n].Pos[Axis1]-Xmin;
-      pos[1]= P[n].Pos[Axis2]-Ymin;
-
-
-      h = Hsml[n];
-      
-      if(h < hmin)
-        h = hmin;
-
-      if(h > hmax)
-        h = hmax;
-
-#ifdef PERIODIC
-      if((NGB_PERIODIC(Xc - P[n].Pos[Axis1]) + 0.5 * (Xmax - Xmin)) < -Hsml[n])
-        continue;
-      if((NGB_PERIODIC(Xc - P[n].Pos[Axis1]) - 0.5 * (Xmax - Xmin)) > Hsml[n])
-        continue;
-      
-      if((NGB_PERIODIC(Yc - P[n].Pos[Axis2]) + 0.5 * (Ymax - Ymin)) < -Hsml[n])
-        continue;
-      if((NGB_PERIODIC(Yc - P[n].Pos[Axis2]) - 0.5 * (Ymax - Ymin)) > Hsml[n])
-        continue;
-#else
-      if(pos[0] + h < 0 || pos[0] - h >  LengthX
-         || pos[1] + h < 0 || pos[1] - h > LengthY)
-        continue;
-#endif
- 
-      h2 = h * h;
-
-      nx = h / pixelsizeX + 1;
-      ny = h / pixelsizeY + 1;
-
-      /* x,y central pixel of region covered by the particle on the mesh */
-      
-      x = (floor(pos[0] / pixelsizeX) + 0.5) * pixelsizeX;
-      y = (floor(pos[1] / pixelsizeY) + 0.5) * pixelsizeY;
-
-      /* determine kernel normalizaton */      
-      sum = 0;
-
-      for(dx = -nx; dx <= nx; dx++)
-        for(dy = -ny; dy <= ny; dy++)
-          {
-            xx = x + dx * pixelsizeX - pos[0];
-            yy = y + dy * pixelsizeY - pos[1];
-            r2 = xx * xx + yy * yy;
-            
-            if(r2 < h2)
-              {
-                r = sqrt(r2);
-                u = r / h;
-
-		// The following is for a Cubic Spline kernel. 
-                if(u < 0.5)
-                  wk = (2.546479089470 + 15.278874536822 * (u - 1) * u * u);
-                else
-                  wk = 5.092958178941 * (1.0 - u) * (1.0 - u) * (1.0 - u);
-				
-		// The following is for a Wendland C2 kernel. 
-		//wk = 3.342253804929802 * pow(1.0 - u, 4.0) * (1.0 + (4.0 * u));
-                
-                sum += wk;
-              }
-          }
-      
-      if(sum < 1.0e-10)
-        continue;
-
-      for(dx = -nx; dx <= nx; dx++)
-        for(dy = -ny; dy <= ny; dy++)
-          {
-            xxx = x + dx * pixelsizeX;
-            yyy = y + dy * pixelsizeY;
-
-#ifdef PERIODIC
-            xxx = NGB_PERIODIC(xxx + Xmin - Xc) + Xc - Xmin;
-            yyy = NGB_PERIODIC(yyy + Ymin - Yc) + Yc - Ymin;
-#endif
-           
-            if(xxx >= 0 && yyy >= 0)
-              {
-                i = xxx / pixelsizeX;
-                j = yyy / pixelsizeY;
-                
-                if(i >= 0 && i < Xpixels)
-                  if(j >= 0 && j < Ypixels)
-                    {
-                      xx = x + dx * pixelsizeX - pos[0];
-                      yy = y + dy * pixelsizeY - pos[1];
-                      r2 = xx * xx + yy * yy;
-                      
-                      if(r2 < h2)
-                        {
-                          r = sqrt(r2);
-                          u = r / h;
-
-						  // Cubic Spline kernel. 
-                          if(u < 0.5)
-                            wk = (2.546479089470 + 15.278874536822 * (u - 1) * u * u);
-                          else
-                            wk = 5.092958178941 * (1.0 - u) * (1.0 - u) * (1.0 - u);
-						  
-						  // Wendland C2 kernel. 
-						  //wk = 3.342253804929802 * pow(1.0 - u, 4.0) * (1.0 + (4.0 * u)); 
-                          
-                          Value[i * Ypixels + j] += Mass[n] * wk / sum;
-                          ValueQuantity[i * Ypixels + j] += Mass[n]*Quantity[n]*wk / sum;
-                        }
-                    }
-              }
-          }
+    //printf("%ld..  xy=%g|%g  weight=%g wt(1|2|3)=%g|%g|%g  h=%g  imin/max=%ld|%ld  jmin/max=%ld|%ld  \n",
+        //n,x[n],y[n],Mass[n],quantity[n],wt2[n],wt3[n],h,imin,imax,jmin,jmax); fflush(stdout);
+//    printf("%ld..  xy=%g|%g  weight(1|2|3)=%g|%g|%g  h=%g  imin/max=%ld|%ld  jmin/max=%ld|%ld  wt_sum=%g \n",
+//        n,x[n],y[n],Mass1[n],Mass2[n],Mass3[n],h,imin,imax,jmin,jmax,wt_sum); fflush(stdout);
     }
+    
+  } // for(n=0;n<N_xy;n++)
+  
+  
+  return 1;
+} // closes main program 
 
-
-  for(i = 0; i < Xpixels; i++)
-    for(j = 0; j < Ypixels; j++)
-      if(Value[i * Ypixels + j]>0)
-        ValueQuantity[i * Ypixels + j] /= Value[i * Ypixels + j]; // divide by deposited mass
-
-
-  //printf("\n");
-}
 
