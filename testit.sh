670dfb9a5cdffcc7ea48e64cc2322a419337c33c
Alex Gurvich <agurvich@u.northwestern.edu>
2021-01-04 15:01:56 -0600

    reintroduce command line syntax for render loops

--- testit.sh
+++ testit.sh
@@ -1,2 +1,7 @@
 curdir=`pwd`
-python firestudio/gas_movie_maker.py --snapdir="${HOME}/snaps/m12i_res7100/output" --snapstart=600 --snapmax=600 --frame_half_width=5 --frame_depth=5 --datadir=${curdir} --multiproc=0 --extract_galaxy=1 --noaxis=1 --edgeon=0 --pixels=1200
+snapdir="${HOME}/snaps/metal_diffusion/m12i_res7100/output"
+datadir="${HOME}/scratch/data/metal_diffusion/m12i_res7100"
+snapstart=71
+snapmax=600
+
+python firestudio/gas_movie_maker.py --snapdir=${snapdir} --snapstart=${snapstart} --snapmax=${snapmax} --frame_half_width=15 --frame_half_thickness=15 --datadir=${datadir} --multiproc=20 --noaxis=1 --edgeon=1 --pixels=1200
