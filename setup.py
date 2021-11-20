dd718a76b618e59648415adcd72fd31c29f2e7ce
Alex Gurvich <agurvich@u.northwestern.edu>
2021-11-20 10:14:49 -0600

    execute make when pip installed to automatically compile .so files

--- setup.py
+++ setup.py
@@ -1,9 +1,52 @@
-import setuptools
+import subprocess
+import os
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+####
+#https://stackoverflow.com/questions/19569557/pip-not-picking-up-a-custom-install-cmdclass
+#BEGIN CUSTOM INSTALL COMMANDS
+#These classes are used to hook into setup.py's install process. Depending on the context:
+#$ pip install my-package
+
+#Can yield `setup.py install`, `setup.py egg_info`, or `setup.py develop`
+
+def custom_command():
+    C_routine_subdir = 'src/firestudio/utils/C_routines'
+    C_routines = os.listdir(C_routine_subdir)
+    for C_routine in C_routines:
+        process = subprocess.Popen(
+            "make",
+            shell=True,
+            cwd=os.path.join(C_routine_subdir,C_routine))
+        process.wait()
+        
+class CustomInstall(install):
+    def run(self):
+        custom_command()
+        install.run(self)
+
+
+class CustomDevelop(develop):
+    def run(self):
+        custom_command()
+        develop.run(self)
+
+
+class CustomEggInfo(egg_info):
+    def run(self):
+        custom_command()
+        egg_info.run(self)
+
+####  END CUSTOM INSTALL COMMANDS 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="firestudio",
     version="1.0.0",
     author = 'Alex Gurvich',
@@ -21,12 +64,17 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=find_packages(where="src"),
     python_requires=">=3.6",
     install_requires=[            
           'abg_python',
           'numpy',
       ],
     include_package_data=True,
+    ## to compile C code
+    cmdclass={
+        'install': CustomInstall,
+        'develop': CustomDevelop,
+        'egg_info': CustomEggInfo},
 )
 
