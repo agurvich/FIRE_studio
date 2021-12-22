bd29644f1596f4200489eef89823c8490b8020bd
Alex Gurvich <agurvich@u.northwestern.edu>
2021-12-22 15:25:29 -0600

    update script to do composition

--- src/firestudio/scripts/make_bursty_movie.py
+++ src/firestudio/scripts/make_bursty_movie.py
@@ -9,12 +9,13 @@ from abg_python.galaxy.gal_utils import Galaxy
 from firestudio.interpolate.interpolate import InterpolationHandler
 from firestudio.studios.gas_studio import GasStudio
 from firestudio.studios.star_studio import StarStudio
+from firestudio.studios.composition import Composition
 
 
 def main(
     coord_interp_mode='cylindrical',
-    name='m12b_res7100',
-    multi_threads=6):
+    name='m12b_res57000',
+    multi_threads=1):
 
     #galaxy = Galaxy('m12b_res7100',600)
     galaxy = Galaxy(name,600)
@@ -35,18 +36,32 @@ def main(
         {'name':galaxy.name,
         'keys_to_extract':['Metallicity','AgeGyr'],
         'final_orientation':True},
-        render_kwargs={
-            'quick':False,'use_metadata':False,
-            'save_meta':True,'assert_cached':False,
+        render_kwargss=[
+            {'weight_name':'Masses',
+            'quantity_name':'Temperature',
+            'min_quantity':2,
+            'max_quantity':7,
+            'quantity_adjustment_function':np.log10,
+            #'min_weight':-0.5,
+            #'max_weight':3, 
+            },
+            {'quick':False,#'use_metadata':True,
+            'save_meta':True,#'assert_cached':False,
             #'min_quantity':2,'max_quantity':7,
             #'min_weight':-0.5,'max_weight':3 
-            }, ## msun/pc^2,
-        studio_kwargs={'maxden':2.2e8,'dynrange':4.7e2,'no_dust':True},
+            }], ## msun/pc^2,
+        studio_kwargss=[
+            {'figure_label':None},
+            {'maxden':2.2e8,
+            'dynrange':4.7e2,
+            'no_dust':True,
+            'scale_bar':False}],
         multi_threads=multi_threads,
-        savefig='halo_file_young_star_%s'%coord_interp_mode,
-        which_studio=StarStudio,
-        check_exists=True, ## skip rendering a frame if the png already exists
-        timestamp=bursty_time) 
+        savefigs=None, ## defaults to GasStudio_frame and StarStudio_frame
+        which_studios=[GasStudio,StarStudio],
+        check_exists=False, ## skip rendering a frame if the png already exists
+        timestamp=bursty_time,
+        add_composition=True)  ## will add a composition frame of the requested Studios
 
 if __name__ == '__main__':
     main()
