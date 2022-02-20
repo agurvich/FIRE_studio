6b26aff4f32113d6b4c2f849ef06eb973da747d8
Alex Gurvich <agurvich@u.northwestern.edu>
2022-02-20 11:31:32 -0600

    add command line args

--- src/firestudio/scripts/make_bursty_movie.py
+++ src/firestudio/scripts/make_bursty_movie.py
@@ -1,9 +1,10 @@
 import sys
+import getopt
 
 import numpy as np
 
 from abg_python.plot_utils import plt
-plt.rcParams['figure.dpi']=240
+plt.rcParams['figure.dpi']=300
 from abg_python.galaxy.gal_utils import Galaxy
 
 from firestudio.interpolate.interpolate import InterpolationHandler
@@ -13,27 +14,36 @@ from firestudio.studios.composition import Composition
 
 
 def main(
-    coord_interp_mode='cylindrical',
     name='m12b_res57000',
-    multi_threads=30):
+    multi_threads=1,
+    take_avg_L = True 
+    ):
+
 
     #galaxy = Galaxy('m12b_res7100',600)
     galaxy = Galaxy(name,600)
     _,bursty_time,_,_ = galaxy.get_bursty_regime(save_meta=True)
+    galaxy.get_snapshotTimes()
+    bursty_snap_index = np.argmin((galaxy.snap_gyrs-bursty_time)**2)
+
 
     interp_handler = InterpolationHandler(
-        120,
-        bursty_time-2,
-        bursty_time+2,
-        camera_pos=[0,0,30],
-        scale_line_length=10,
+        30,
+        galaxy.snap_gyrs[bursty_snap_index]-1,
+        galaxy.snap_gyrs[bursty_snap_index]-1e-3,#+2,
+        camera_pos=[0,0,15],
+        scale_line_length=5,
         #time_slice=slice(-24*4,None),
         #time_slice=slice(-10,None),
-        coord_interp_mode=coord_interp_mode,
+        time_slice=slice(0,100)
     )
 
+    savefig = 'avg_L' if take_avg_L else 'rot_jhat'
+    savefig+='_child'
     figs = interp_handler.interpolateAndRender(
         {'name':galaxy.name,
+        ##'ABG_force_multithread':60,
+        'take_avg_L':take_avg_L,
         'keys_to_extract':['Metallicity','AgeGyr'],
         'final_orientation':True,
         'loud_metadata':False},
@@ -44,18 +54,21 @@ def main(
             'max_quantity':7,
             'quantity_adjustment_function':np.log10,
             'save_meta':True,
-            #'use_metadata':False,
+            'use_metadata':False,
             #'assert_cached':False,
             #'min_weight':-0.5,
             #'max_weight':3, 
             },
             {'quick':False,#'use_metadata':True,
             'save_meta':True,#'assert_cached':False,
-            #'use_metadata':False,
+            'use_metadata':False,
             #'min_quantity':2,'max_quantity':7,
             #'min_weight':-0.5,'max_weight':3 
             },
-            {}], ## msun/pc^2,
+            {
+            #'save_meta':False,#'assert_cached':False,
+            'use_metadata':False,
+            }], ## msun/pc^2,
         studio_kwargss=[
             {
             'savefig':None
@@ -77,7 +90,25 @@ def main(
         which_studios=[GasStudio,StarStudio,StarStudio],
         check_exists=True, ## skip rendering a frame if the png already exists
         timestamp=bursty_time,
-        add_composition='bursty_movie')  ## will add a composition frame of the requested Studios
+        add_composition=savefig)  ## will add a composition frame of the requested Studios
 
 if __name__ == '__main__':
-    main()
+    argv = sys.argv[1:]
+    opts,args = getopt.getopt(
+        argv,'',[
+        'name=',
+        'multi_threads=',
+        'take_avg_L='
+        ])
+
+    for i,opt in enumerate(opts):
+        if opt[1]=='':
+            opts[i]=('mode',opt[0].replace('-',''))
+        else:
+            try:
+                ## if it's an int or a float this should work
+                opts[i]=(opt[0].replace('-',''),eval(opt[1]))
+            except:
+                ## if it's a string... not so much
+                opts[i]=(opt[0].replace('-',''),opt[1])
+    main(**dict(opts))
