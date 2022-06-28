296e44a939a42c171f0a7e37613eb3fb8840e2e5
Alex Gurvich <agurvich@u.northwestern.edu>
2022-06-27 21:54:59 -0500

    use readthedocs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,12 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
+
+import firestudio
 
 
 # -- Project information -----------------------------------------------------
@@ -30,8 +36,10 @@ release = '2.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary','sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx','sphinx.ext.autosectionlabel','nbsphinx']
+
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +55,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+pygments_style = 'sphinx'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
